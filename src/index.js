6222a79a9f1822a6f412380f18589d9b22cd4c82
Maha Magdy <mahamagdy3@gmail.com>
2021-08-26 15:55:33 +0200

    Add the styles of the app, and sorting the menu by the index of the list item

--- src/index.js
+++ src/index.js
@@ -11,15 +11,25 @@ let toDoTasks = [
 ];
 
 function toDoList(list) {
+  list.sort((a, b) => (a.index > b.index ? 1 : -1));
+
   const listContainer = document.getElementById("to-do-list");
 
   list.map((task) => {
     const li = document.createElement("li");
     const description = document.createElement("p");
     const descriptionText = document.createTextNode(task.description);
-
     description.appendChild(descriptionText);
+
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+
+    const btn = document.createElement("button");
+
+    li.appendChild(checkbox);
     li.appendChild(description);
+    li.appendChild(btn);
+
     listContainer.appendChild(li);
   });
 }

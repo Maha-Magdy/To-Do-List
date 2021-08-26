4cf6a841dc709e42ff0774b9d6930486e565983c
Maha Magdy <mahamagdy3@gmail.com>
2021-08-26 02:01:05 +0200

    create the basic script to parse the to-do-list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import _ from 'lodash';
-import './style.css';
+import lodash from "lodash";
+import "./style.css";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+let toDoTasks = [
+  { description: "wash the dishes", completed: false, index: 0 },
+  {
+    description: "complete list structure milestone",
+    completed: false,
+    index: 1,
+  },
+];
+
+function toDoList(list) {
+  const listContainer = document.getElementById("to-do-list");
+
+  list.map((task) => {
+    const li = document.createElement("li");
+    const description = document.createElement("p");
+    const descriptionText = document.createTextNode(task.description);
+
+    description.appendChild(descriptionText);
+    li.appendChild(description);
+    listContainer.appendChild(li);
+  });
+}
+
+window.addEventListener("load", toDoList(toDoTasks));

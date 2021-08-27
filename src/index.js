4e389f3eda6329c2fb5fe988a9be57d6be169051
Maha Magdy <mahamagdy3@gmail.com>
2021-08-27 19:52:33 +0200

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable import/no-named-default */
-import "./style.css";
-import { default as Task, updateStatus, updateDescription } from "./task.js";
-import handleStorage from "./handle-storage.js";
+/* eslint-disable no-use-before-define */
+import './style.css';
+import { default as Task, updateStatus, updateDescription } from './task.js';
+import handleStorage from './handle-storage.js';
 
 let toDoTasks = handleStorage.getToDoList();
-const listContainer = document.getElementById("to-do-list");
+const listContainer = document.getElementById('to-do-list');
 
 function toDoList(list) {
   list.sort((a, b) => (a.index > b.index ? 1 : -1));
 
-  listContainer.innerHTML = "";
+  listContainer.innerHTML = '';
 
   list.map((task) => {
-    const li = document.createElement("li");
-    const description = document.createElement("p");
+    const li = document.createElement('li');
+    const description = document.createElement('p');
     const descriptionText = document.createTextNode(task.description);
     description.appendChild(descriptionText);
 
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
     if (task.completed) {
       checkbox.checked = true;
     } else {
       checkbox.checked = false;
     }
 
-    checkbox.addEventListener("change", (e) => {
+    checkbox.addEventListener('change', (e) => {
       let completed;
       if (e.target.checked) {
         completed = true;
@@ -36,7 +37,7 @@ function toDoList(list) {
 
       const index = Array.prototype.indexOf.call(
         listContainer.childNodes,
-        e.target.offsetParent
+        e.target.offsetParent,
       );
 
       const allTasks = handleStorage.getToDoList();
@@ -49,11 +50,11 @@ function toDoList(list) {
       handleStorage.updateToDoList(allTasks);
     });
 
-    const btn = document.createElement("button");
-    btn.addEventListener("click", (e) => {
+    const btn = document.createElement('button');
+    btn.addEventListener('click', (e) => {
       const index = Array.prototype.indexOf.call(
         listContainer.childNodes,
-        e.target.offsetParent
+        e.target.offsetParent,
       );
 
       const allTasks = handleStorage.getToDoList();
@@ -70,16 +71,16 @@ function toDoList(list) {
   });
 }
 
-window.addEventListener("load", toDoList(toDoTasks));
+window.addEventListener('load', toDoList(toDoTasks));
 
-const resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", () => {
+const resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', () => {
   handleStorage.resetToDoList();
   toDoTasks = handleStorage.getToDoList();
   toDoList(toDoTasks);
 });
 
-const newTask = document.getElementById("new-task");
+const newTask = document.getElementById('new-task');
 
 function addNewTask() {
   if (newTask.value) {
@@ -87,17 +88,17 @@ function addNewTask() {
     const task = new Task(taskDescription, false, 0);
     handleStorage.setTask(task);
     toDoTasks = handleStorage.getToDoList();
-    newTask.value = "";
+    newTask.value = '';
     toDoList(toDoTasks);
   }
 }
 
-const addToYourList = document.getElementById("add-to-your-list");
+const addToYourList = document.getElementById('add-to-your-list');
 
-addToYourList.addEventListener("click", addNewTask);
+addToYourList.addEventListener('click', addNewTask);
 
-newTask.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+newTask.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     addNewTask();
   }
 });
@@ -107,17 +108,17 @@ function updateOrDeleteTask(li, index, task) {
   li.removeChild(li.lastElementChild);
 
   // handling update functionality
-  let input = document.createElement("input");
-  input.setAttribute("type", "text");
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
   input.value = task.description;
 
-  input.addEventListener("change", (e) => {
+  input.addEventListener('change', (e) => {
     const index = Array.prototype.indexOf.call(
       listContainer.childNodes,
-      e.target.offsetParent
+      e.target.offsetParent,
     );
 
-    let allTasks = handleStorage.getToDoList();
+    const allTasks = handleStorage.getToDoList();
     const selectedTask = allTasks[index];
 
     const task = updateDescription(selectedTask, e.target.value);
@@ -132,16 +133,15 @@ function updateOrDeleteTask(li, index, task) {
   li.appendChild(input);
 
   // handling delete functionality
-  let deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "delete-button");
-  deleteBtn.addEventListener("click", (e) => {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'delete-button');
+  deleteBtn.addEventListener('click', (e) => {
     const index = Array.prototype.indexOf.call(
       listContainer.childNodes,
-      e.target.offsetParent
+      e.target.offsetParent,
     );
 
-    let allTasks = handleStorage.getToDoList();
-    const selectedTask = allTasks[index];
+    const allTasks = handleStorage.getToDoList();
 
     allTasks.splice(index, 1);
 
@@ -153,10 +153,9 @@ function updateOrDeleteTask(li, index, task) {
   li.appendChild(deleteBtn);
 }
 
-const clearCompletedTasksBtn = document.getElementById("clear-completed-tasks");
-clearCompletedTasksBtn.addEventListener("click", () => {
+const clearCompletedTasksBtn = document.getElementById('clear-completed-tasks');
+clearCompletedTasksBtn.addEventListener('click', () => {
   toDoTasks = handleStorage.getToDoList();
-  console.log("toDoTasks is", toDoTasks);
   const unCompletedTasks = toDoTasks.filter((task) => (task.completed === false));
   handleStorage.updateToDoList(unCompletedTasks);
   toDoList(unCompletedTasks);
